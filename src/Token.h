e5c1cfc87c620e54a2b74af2f10372d00e2c4b0e
jooarago <joao.sales.aragao@gmail.com>
2025-03-16 12:41:03 -0300

    literal is variant

--- src/Token.h
+++ src/Token.h
@@ -1,16 +1,18 @@
 #include <string>
+#include <variant>
 #include "TokenType.h"
 
 using namespace std;
+typedef variant<double, string> Literal
 
 class Token {
     public:
         const TokenType type;
         const string lexeme;
-        const string literal;
+        const Literal literal;
         const int line;
 
-        Token(TokenType type, string lexeme, string literal, int line):
+        Token(TokenType type, string lexeme, Literal literal, int line):
             type(type), lexeme(lexeme), literal(literal), line(line) {}
             
         /* toString aqui */
